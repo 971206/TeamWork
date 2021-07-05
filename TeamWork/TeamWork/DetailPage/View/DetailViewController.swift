aac6380735fd5a4cc997afa6c9d744fbb17d9b27
MacBook <>
2021-07-05 10:24:05 +0400

    detail page

--- TeamWork/TeamWork/DetailPage/View/DetailViewController.swift
+++ TeamWork/TeamWork/DetailPage/View/DetailViewController.swift
@@ -9,10 +9,16 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet weak var labelContent: UILabel!
+    @IBOutlet weak var labelDate: UILabel!
+    @IBOutlet weak var labelAuthorName: UILabel!
+    @IBOutlet weak var imageAuthor: UIImageView!
+    @IBOutlet weak var labelTitle: UILabel!
+    @IBOutlet weak var image: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        imageAuthor.makeRounded()
     }
     
 
@@ -26,4 +32,16 @@ class DetailViewController: UIViewController {
     }
     */
 
+}
+
+extension UIImageView {
+
+    func makeRounded() {
+
+        self.layer.borderWidth = 1
+        self.layer.masksToBounds = false
+        self.layer.borderColor = UIColor.black.cgColor
+        self.layer.cornerRadius = self.frame.height / 2
+        self.clipsToBounds = true
+    }
 }
